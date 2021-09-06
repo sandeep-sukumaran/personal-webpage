28b7d13deaeeeea449955a79c20ae31017433526
Sandeep Sukumaran <sandeepsukumaran@protonmail.com>
2021-09-06 12:14:26 -0500

    feat: initial commit of React files

---
+++ my-page/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          WIP
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/sandeep-sukumaran"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
